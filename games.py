7e5776557df62fb962c40deab31308fd41ac968c
Gabriel Romero <gabester78@gmail.com>
2020-07-23 17:03:35 -0500

    Clean up the code a bit.

--- games.py
+++ games.py
@@ -6,23 +6,43 @@ money = 100
 
 
 def coin_flip(guess, bet):
+    print('')
+
+    # checks to make sure player doesn't have any typos in their guess
+    # if guess != 'Heads' or 'Tails':
+    #     print('You must choose Heads or Tails to play coin flip!')
+    #     return 0
+
+    # checks to make sure player has money to bet
+    if money <= 0:
+        print('You do not have enough money to play. Game over!')
+
+        return 0
+
+    # checks to make sure player is betting at least $1
     if bet <= 0:
         print('Your coin flip bet must exceed $0.')
+
         return 0
+
     print("Your coin flip bet is $" + str(bet) +
           " and your guess is " + guess + '.')
+
+    # returns a random 1 or 2
     flip = random.randint(1, 2)
+
+    # prints out the result of the random number found
     if flip == 1:
         result = 'Heads'
-        print('And the winner is ' + result + '!')
+        print('The result is ' + result + '!')
     elif flip == 2:
         result = 'Tails'
-        print('And the winner is ' + result + '!')
+        print('The result is ' + result + '!')
 
+    # prints out whether the player won or lost their bet
     if result == guess:
-        total = bet
-        print('You won $' + str(total) + '!')
-        return total
+        print('You won $' + str(bet) + '!')
+        return bet
     elif result != guess:
         total = bet * -1
         print('You lost $' + str(abs(total)) + '!')
@@ -30,7 +50,11 @@ def coin_flip(guess, bet):
 
 
 # Call your game of chance functions here
-money += coin_flip('Heads', 1)
-money += coin_flip('Tails', 5)
-money += coin_flip('Tails', 10)
-print('Your current total is $' + str(money) + '!')
+print('')
+print('Your current total in the bank is $' + str(money) + '!')
+money += coin_flip('Heads', 20)
+money += coin_flip('Heads', 30)
+money += coin_flip('Heads', 50)
+print('')
+print('Your current total in the bank is $' + str(money) + '!')
+print('')
